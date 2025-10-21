e81e411ce1d7a43089fecf5178fab679c0ba4da0
Ashani <ashanitharukshi2002@gmail.com>
2025-10-21 16:36:27 +0530

    fix: add missing import for Navbar component and ensure proper formatting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from './context/ThemeContext'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       </div>
     </ThemeProvider>
     
-  )
-}
+  );
+};
 
-export default App
+export default App;
