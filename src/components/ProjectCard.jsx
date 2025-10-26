7180f386d48313d9b0fe5939cac39019292de026
Ashani <ashanitharukshi2002@gmail.com>
2025-10-26 23:20:21 +0530

    feat: implement ProjectCard component with animation and props for project display

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { FiGithub } from 'react-icons/fi'
 
-const ProjectCard = () => {
+
+const ProjectCard = ({project, index, isDarkMode}) => {
+    const cardVariants ={
+        hidden:{y:20, opacity:0},
+        visible:{
+            y:0,
+            opacity:1,
+            transition:{
+                duration:0.6,
+                ease:"easeOut",
+            },
+        },
+
+    };
   return (
     <div>ProjectCard</div>
   )
