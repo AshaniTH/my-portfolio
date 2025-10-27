f131debb0d21d02328613e7befe00036413258a1
Ashani <ashanitharukshi2002@gmail.com>
2025-10-28 00:21:43 +0530

    feat: add AboutSection component to enhance application structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import HeroSection from './components/Sections/HeroSection';
 import SkillsSection from './components/Sections/SkillsSection';
 import ProjectSection from './components/Sections/ProjectSection';
+import AboutSection from './components/Sections/AboutSection';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <HeroSection />
         <SkillsSection />
         <ProjectSection />
+        <AboutSection />
        
       </div>
     </ThemeProvider>
